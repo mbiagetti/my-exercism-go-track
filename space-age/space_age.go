21e8266b849fc8111ece4900f438a29caac23c91
Matteo Biagetti <matteo.biagetti@gmail.com>
2021-02-22 09:06:20 +0100

    Performance improvements

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -2,24 +2,28 @@ package space
 
 type Planet interface{}
 
-/*
-var aaa = map[Planet] float64 {
-	"Earth": float64(1),
-}
-*/
 func Age(seconds float64, planet Planet) float64 {
-	earth := seconds / float64(31557600)
+	var age float64
 
-	var m = make(map[Planet]float64)
+	switch planet {
+	case "Earth":
+		age = float64(1)
+	case "Mercury":
+		age = float64(0.2408467)
+	case "Venus":
+		age = float64(0.61519726)
+	case "Mars":
+		age = float64(1.8808158)
+	case "Jupiter":
+		age = float64(11.862615)
+	case "Saturn":
+		age = float64(29.447498)
+	case "Uranus":
+		age = float64(84.016846)
+	case "Neptune":
+		age = float64(164.79132)
 
-	m["Earth"] = float64(1)
-	m["Mercury"] = float64(0.2408467)
-	m["Venus"] = float64(0.61519726)
-	m["Mars"] = float64(1.8808158)
-	m["Jupiter"] = float64(11.862615)
-	m["Saturn"] = float64(29.447498)
-	m["Uranus"] = float64(84.016846)
-	m["Neptune"] = float64(164.79132)
+	}
 
-	return earth / m[planet]
+	return seconds / float64(31557600) / age
 }
